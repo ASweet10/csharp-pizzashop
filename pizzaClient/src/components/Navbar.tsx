838e073386d11685d48686460d68f38c845b6ac8
ASweet10 <31268208+ASweet10@users.noreply.github.com>
2025-06-27 13:09:25 -0500

    Cart fix
    Now only displays total if items are in cart

--- pizzaClient/src/components/Navbar.tsx
+++ pizzaClient/src/components/Navbar.tsx
@@ -19,11 +19,13 @@ function Navbar() {
   return (
     <nav className="fixed justify-between top-0 bg-transparent px-8 md:px-20 h-28 z-50 w-[100vw]">
       <div className='flex items-center justify-between pt-4'>
+
         <div className='flex items-center md:gap-3'>
           <Link to="/" className={`text-4xl px-4 py-2 font-heading ${location.pathname === "/" ? "text-yellow-400" : "text-white"}`}>Tony's Pizza</Link>
           {/* <img src={pizzaImg} alt="" className='animate-spin animate-duration-2000 animate-repeat-infinite h-20 w-20' id="spinner"/> */}
           <Link to="/menu" className={`text-4xl px-4 py-2 font-heading ${location.pathname === "/menu" ? "text-yellow-400" : "text-white"}`}>Menu</Link>
         </div>
+
         <div className="relative">
           <button onClick={() => setCartOpen(!cartOpen)} className="relative items-center flex gap-2 text-2xl cursor-pointer text-gray-200">
             <FaShoppingCart /> ({totalItemCount})
@@ -34,41 +36,41 @@ function Navbar() {
               {cartItems.length === 0 ? (
                 <p>Your cart is empty.</p>
               ) : (
-                cartItems.map(item => (
-                  <div key={`${item.type}-${item.id}`} className="flex mb-2 gap-2 w-full">
-                    <div className='flex flex-row w-full items-center gap-4'>
-                      <span className='text-lg font-bold'>{item.quantity} {item.name}</span>
-                      <div className='flex items-center gap-4'>
-                        <span className=''>${(item.quantity * item.price).toFixed(2)}</span>
-                        <button onClick={() => handleRemoveFromCart(item)}
-                          className='bg-red-600 text-white px-4 py-2 rounded text-xl h-12 cursor-pointer'
-                        >
-                          X
-                        </button>
+                <>
+                  { cartItems.map(item => (
+                    <div key={`${item.type}-${item.id}`} className="flex mb-2 gap-2 w-full">
+                      <div className='flex flex-row w-full items-center gap-4'>
+                        <span className='text-lg font-bold'>{item.quantity} {item.name}</span>
+                        <div className='flex items-center gap-4'>
+                          <span className=''>${(item.quantity * item.price).toFixed(2)}</span>
+                          <button onClick={() => handleRemoveFromCart(item)}
+                            className='bg-red-600 text-white px-4 py-2 rounded text-xl h-12 cursor-pointer'
+                          >
+                            X
+                          </button>
+                        </div>
                       </div>
                     </div>
+                  ))}
+
+                  <div className="flex gap-4 mt-4 text-xl font-bold">
+                    <span>Total:</span>
+                    <span>${totalItemPrice.toFixed(2)}</span>
                   </div>
-                ))
+
+                  <hr className="my-2" />
+                  
+                  <button onClick={() => { setCartOpen(false); navigate('/checkout'); }}
+                    className="bg-green-600 text-white px-4 py-2 rounded text-lg mt-2 cursor-pointer"
+                  >
+                    Checkout
+                  </button>
+                </>
               )}
-              <div className="flex gap-4 mt-4 text-xl font-bold">
-                <span>Total:</span>
-                <span>${totalItemPrice.toFixed(2)}</span>
-              </div>
-              <hr className="my-2" />
-              <button
-                onClick={() => {
-                  setCartOpen(false);
-                  navigate('/checkout');
-                }}
-                className="bg-green-600 text-white px-4 py-2 rounded text-lg mt-2 cursor-pointer"
-              >
-                Checkout
-              </button>
             </div>
           )}
         </div>
       </div>
-
     </nav>
   );
 }
